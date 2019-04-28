69511993a62bed16e62dd14fea10697f348eb028
chrismar303 <chrismar303@gmail.com>
2019-04-28 02:51:43 -0700

    Modified: app.js | Created: views/ views/home.ejs
    Added express and ejs to 'app.js'. Added home page route and set it to
    'home.ejs'.
    
    Created 'views/' folder for ejs files and templates.

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const {PythonShell} = require('python-shell');
+const express = require('express');
+const app = express();
+const port = 3000;
 
-PythonShell.run("scan_class.py", {args: [34130]}, function(err, results) {
-    if(err) throw err;
-    console.log(`Results: ${results[1]}` );
+// listen on '/'
+app.get('/', function(req, res) {
+    res.render('home.ejs');
 });
+
+// setup app to listen for routes
+app.listen(port, () => console.log(`listening on port: ${port}`));
+
+function scan_class(course_code) {
+    PythonShell.run("scan_class.py", {args: [course_code]}, function(err, results) {
+        if(err) throw err;
+        console.log(`Results: ${results[1]}` );
+    });
+}
