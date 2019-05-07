5183ff6d658cf72badc7ac51de4c6a17c9e59602
chrismar303 <chrismar303@gmail.com>
2019-05-07 02:16:40 -0700

    Created: result.ejs | Added "/search" route
    * result.ejs corresponds to route "/search"
    - Will search if course code has changed to OPEN
    - returns a prototype of registration form for notifications
    * Added extra padding to top of page
    * Added meta tag to fix issue chrome had with routes
    * Added body parser to project

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
 const {PythonShell} = require('python-shell');
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const port = 3000;
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 
 // listen on '/'
 app.get('/', function(req, res) {
-    res.render('home.ejs');
+    res.render('home');
 });
+
+// class search
+app.post('/search', function(req, res) {
+    const course_code = req.body.course_code
+    scan_class(course_code);
+    res.render('result');
+});
+
+// TODO: '/addclass' route
 
 // setup app to listen for routes
 app.listen(port, () => console.log(`listening on port: ${port}`));
 
+// parse schedule of classes
 function scan_class(course_code) {
     PythonShell.run("scan_class.py", {args: [course_code]}, function(err, results) {
         if(err) throw err;
