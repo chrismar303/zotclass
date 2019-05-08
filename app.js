50e69b735abd4a5a35df8c9b97a83de24b3dad70
chrismar303 <chrismar303@gmail.com>
2019-05-08 03:24:58 -0700

    Modified /search & script | renamed result.ejs
    * Changed /search route to /course_info
    * Renamed result.ejs to course_info.ejs
    * Added more custom styles to app.css
    * Modified scan_class.py script to include course title and removed
    unicode character
    * Modified app.js to call scan_class.py and to pass course_info to
    result.ejs
    - added /addclass route, but it currently does nothing
    * result.ejs now displays course information

--- app.js
+++ app.js
@@ -14,21 +14,25 @@ app.get('/', function(req, res) {
 });
 
 // class search
-app.post('/search', function(req, res) {
+app.post('/course_info', function(req, res) {
     const course_code = req.body.course_code
-    scan_class(course_code);
-    res.render('result');
+    scan_class(course_code,res); // scans and loads 'result.ejs'
 });
 
 // TODO: '/addclass' route
+app.post('/addclass', function(req, res) {
+
+});
 
 // setup app to listen for routes
 app.listen(port, () => console.log(`listening on port: ${port}`));
 
 // parse schedule of classes
-function scan_class(course_code) {
+function scan_class(course_code, res) {
     PythonShell.run("scan_class.py", {args: [course_code]}, function(err, results) {
         if(err) throw err;
-        console.log(`Results: ${results[1]}` );
+        const data = results[0].replace(/[']/g, "\""); // remove '' from keys in object
+        const course_info = JSON.parse(data);
+        res.render('course_info.ejs', {course_info: course_info}); // load ejs file
     });
 }
--- scan_class.py
+++ scan_class.py
@@ -1,5 +1,6 @@
 import sys
 import urllib.request
+import unicodedata
 from bs4 import BeautifulSoup
 
 # get class code [optional: add current quarter selection]
@@ -13,18 +14,21 @@ with urllib.request.urlopen(url) as response:
 
 # use beautiful soup to parse course data
 soup = BeautifulSoup(html_page, 'html.parser')
-course_div = soup.find('div', attrs={'class': 'course-list'})
-course_row = course_div.table.find_all('tr')[4]
+course_title = unicodedata.normalize("NFKD", soup.find('td', attrs={'class': 'CourseTitle'}).text)
+course_title = " ".join([token for token in course_title.split() if "(" not in token]) # filter out '(co-courses) (prerequiste)'
+course_div = soup.find('div', attrs={'class': 'course-list'}) # get parent tag containing course information
+course_row = course_div.table.find_all('tr')[4]     # get row containing data
 course_data = course_row.find_all('td')
 
 # organize into course information
 course_info = {
     'code': course_data[0].text,
+    'title': course_title,
     'instructor': course_data[4].text,
-    'time': course_data[5].text,
+    'time': unicodedata.normalize("NFKD", course_data[5].text),
     'location': course_data[6].text,
     'status': course_data[-1].text
 }
 
 print(course_info)
-print("CLASS IS OPEN") if course_info['status'] == 'OPEN' else print('NOT OPEN')
+# print("CLASS IS OPEN") if course_info['status'] == 'OPEN' else print('NOT OPEN')
