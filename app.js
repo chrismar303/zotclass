7c9666eb0e1c622d46cd98517e3cdb045d010cfa
chrismar303 <chrismar303@gmail.com>
2019-05-08 21:23:39 -0700

    Changed /class routes | Added MongoDB w/Atlas
    * Changed /course_info to /class
    * Changed /addclass to /class/new
    - Changed routes to conform with CRUD formatting
    * Renamed views/course_info.ejs to views/class.ejs
    * Modified app.js
    - Added MongoDB with Atlas Cloud
    - Added notification registration functionality. Users can now enter
    their email and it will be saved with an associated list of course codes
    - renamed function scan_class to scanClass
    * Created a configuration file (config.js) to hide sensitive information
    - app.js interacts with config.js module
    * Added config.js to .gitignore

--- app.js
+++ app.js
@@ -1,38 +1,72 @@
+// module imports
 const {PythonShell} = require('python-shell');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const port = 3000;
+const mongoClient = require('mongodb').MongoClient;
+const config = require('./config');
 
+// localhost port
+const port = 3000;
+// Atlas cloud URL
+const dbinfo = {
+    name: config.getDBName(),
+    url: config.getDBConnectionURL(),
+    collection: config.getCollectionName()
+}
+
+// configuration
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
+/*============= ROUTES  =============*/
 // listen on '/'
 app.get('/', function(req, res) {
     res.render('home');
 });
 
 // class search
-app.post('/course_info', function(req, res) {
-    const course_code = req.body.course_code
-    scan_class(course_code,res); // scans and loads 'course_info.ejs'
+app.post('/class', function(req, res) {
+    const course_code = req.body.course_code;
+    scanClass(course_code, res); // scans and loads 'class.ejs'
 });
 
-// TODO: '/addclass' route
-app.post('/addclass', function(req, res) {
-    res.send('<h1>Add Class</h1>')
+app.post('/class/new', function(req, res) {
+    const user = {email: req.body.email, courseCode: req.body.code};
+    addClass(user, dbinfo);
+    res.redirect('/');
 });
+
 
 // setup app to listen for routes
 app.listen(port, () => console.log(`listening on port: ${port}`));
 
+/*========== Utility Functions ==========*/
 // parse schedule of classes
-function scan_class(course_code, res) {
+function scanClass(course_code, res) {
     PythonShell.run("scan_class.py", {args: [course_code]}, function(err, results) {
         if(err) throw err;
         const data = results[0].replace(/[']/g, "\""); // remove '' from keys in object
         const course_info = JSON.parse(data);
-        res.render('course_info.ejs', {course_info: course_info}); // load ejs file
+        res.set('course_code', [course_info.code]);
+        res.render('class', {course_info: course_info}); // load ejs file
+    });
+}
+
+// add class code to database
+function addClass(user, dbinfo) {
+    mongoClient.connect(dbinfo.url, {useNewUrlParser: true}, function(err, dbref) {
+        if(err) throw err;
+        // get db obj
+        const db = dbref.db(dbinfo.name);
+        // check if user exist first
+        const query = {email: user.email};
+        // change to be made (unique course codes)
+        const update = {$addToSet: {courseCodes: user.courseCode}};
+        // add new document if it does not exist
+        const options = { upsert: true };
+        db.collection(dbinfo.collection).updateOne(query, update, options);
+        dbref.close();
     });
 }
